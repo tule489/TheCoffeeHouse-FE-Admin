8347995eae4d3b2f43de10781263f4ff3a69024d
tule489 <leminhtu489@gmail.com>
2023-06-13 19:08:22 +0700

    Change api url

--- src/environment/domainName.js
+++ src/environment/domainName.js
@@ -1,4 +1,4 @@
-// const domainName = 'https://thecoffeehousebe-production.up.railway.app'
-const domainName = 'http://localhost:8080'
+const domainName = 'https://thecoffeehousebe-production.up.railway.app'
+// const domainName = 'http://localhost:8080'
 
 export default domainName
