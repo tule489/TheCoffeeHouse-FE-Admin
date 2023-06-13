bbe0de37ce5ac1e5c02c0f9aa7a6e7303c5e402d
tule489 <leminhtu489@gmail.com>
2023-06-13 21:08:07 +0700

    Quick fix 7

--- src/routes.js
+++ src/routes.js
@@ -8,17 +8,17 @@ const Order = React.lazy(() => import('./views/order/order'))
 const Accounts = React.lazy(() => import('./views/accounts/accounts'))
 
 const routes = [
-  { path: '/products', name: 'Quản lý sản phẩm', element: Product },
-  { path: '/category/category', name: 'Danh mục', element: Category },
+  { path: '/products', exact: true, name: 'Quản lý sản phẩm', element: Product },
+  { path: '/category/category', exact: true, name: 'Danh mục', element: Category },
   {
     path: '/category/detailedCategory',
-
+    exact: true,
     name: 'Danh mục chi tiết',
     element: DetailedCategory,
   },
-  { path: '/order', name: 'Quản lý đơn hàng', element: Order },
-  { path: '/stats', name: 'Thống kê doanh thu', element: Stats },
-  { path: '/accounts', name: 'Quản lý tài khoản', element: Accounts },
+  { path: '/order', exact: true, name: 'Quản lý đơn hàng', element: Order },
+  { path: '/stats', exact: true, name: 'Thống kê doanh thu', element: Stats },
+  { path: '/accounts', exact: true, name: 'Quản lý tài khoản', element: Accounts },
 ]
 
 export default routes
